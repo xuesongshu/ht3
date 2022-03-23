5d7fc5f051c7c69d74f68737482e63ef0058d5e7
曹威 <caowei@jcdz.cc>
2022-03-23 09:27:57 +0800

    准备移入新的工程

--- main.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-
-#include "cyu3types.h"
-
-extern CyBool_t g_is_app_active;
-
-#endif
--- uart.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-
-#define UART_ADDRESS 0x40077000
-
-void uart_init();
-
-#endif
